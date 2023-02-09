f513094e27b3593c187dde84aec87fc0e29f62f3
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-02-09 11:02:34 -0800

    added user model with bcrypt hashing

---
+++ server/models/User.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//model for user account, will not be required to exists for an order
+//to go through
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match:[/.+@.+\..+/, 'Must match an email address!'],
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [0-9, "Please include only numbers"],
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    address: {
+        type: String,
+        required: true,
+        //potetntially may have  to seperate out for each line
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Object"
+        }
+    ],
+    cart: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Cart",
+        },
+    ],
+});
+
+//hashes the users password pre save to database for security
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')){
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+//compares hashed password to entered password on login to see if they match
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/models/index.js
@@ -0,0 +1,31 @@
+//need models for:
+// user
+    //email
+    //phone number
+    //name
+    //address
+    //previous orders?
+// cart
+    //product
+    //quantity
+    //purchasePrice
+    //totalPrice
+    //priceWithTax
+    //totalTax
+    //status
+// order
+    //status
+    //user
+    //total
+    //updated at
+    // delivery info
+    // save to user account
+// product
+    //image(s)
+    //description
+    //price
+// address
+// occasion
+    //holiday name
+    //products by foreign key for this occasion
+// merchant?
