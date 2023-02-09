ca9f2243ff845f9167c58519aadb288f82ca13b9
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-02-09 11:59:48 -0800

    added product model with array for possible images price description and a boolean for in stock

---
+++ server/models/Product.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema({
+    images: [{
+        type: String,
+        required: true,
+    }],
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    inStock: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const Product = model("Product", productSchema);
+
+module.exports - Product;
