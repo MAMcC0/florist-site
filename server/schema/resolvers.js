da8e06fbd8d60071675c04fa55cf1dfb6740be3d
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-02-09 21:41:26 -0800

    added queries and mutations for all models except cart and order

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -81,13 +81,178 @@ const resolvers = {
         },
 
     },
-
-    //need create User, updateCart, updateOrder, 
-    // updateProduct, createProduct, createOrder, 
-    // createCategory, updateAddress, login, deleteUser, 
-    // updateUser, createAddress, deleteProduct, deleteCart, 
-    // updateOrderStatus, deleteCategory, deleteOrder, 
+    //TODO: look at how to do password recovery
+    //TODO: look at how to do admin auth
+    //need create updateCartAdd, updateCartDelete, updateOrder, 
+    //  createOrder,  createCart, createAddress,
+    //  deleteCart, updateOrderStatus, deleteOrder
     Mutation: {
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, {email, password}) => {
+            //find users by email
+            const user = await User.findOne({ email });
+
+            if(!user){
+                throw new AuthenticationError('No account with that email exists');
+            };
+
+            const correctPass = await user.isCorrectPassword(password);
+            //checks if password is correct
+
+            if(!correctPass){
+                throw new AuthenticationError('Whoops! Incorrect Password!')
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        deleteUser: async (parent, {_id}) => {
+            //destroys users account based on id
+
+            const user = await User.destroy(
+                { _id },
+                { new: true}
+            );
+            return user;
+        },
+
+        updateUserEmail: async (parent, {_id, newEmail}, context) => {
+            try {
+                if(context.user){
+                    const updatedUser = await User.findOneAndUpdate(
+                        {_id: _id},
+                        {$set: {email: newEmail} },
+                        {new: true},
+                    )
+                    return updatedUser;
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
+        updateUserAddress: async (parent, {_id, newAddress}, context) => {
+            try {
+                if(context.user){
+                    const updatedUser = await User.findOneAndUpdate(
+                        {_id: _id},
+                        {$push: {address: newAddress} },
+                        {new: true},
+                    )
+                    return updatedUser;
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
+        createProductListing: async (parent, { productInfo, categoryInfo }, context) => {
+            try {
+                if(context.user){
+                    const productData = await Product.create({
+                        images: [productInfo.images], 
+                        price: productInfo.price,
+                        description: productInfo.description,
+                    });
+
+                    const updatedCategoryData = await Category.findOneAndUpdate(
+                        {_id: categoryInfo._id},
+                        { $push: {products: productData._id}},
+                        {new:true},
+                    ).populate('products')
+                    return updatedCategoryData;
+                    
+                };
+            } catch (error) {
+                console.log(error)
+            };
+        },
+
+        updateProductListing: async (parent, { updatedInfo }, context) => {
+            try {
+                if(context.user){
+                    const updatedProductData = await Product.findOneAndUpdate(
+                        {_id: upddatedInfo._id},
+                        {$set: {
+                            images: [ updatedInfo.images ],
+                            price: updatedInfo.price,
+                            description: updatedInfo.description,
+                        }},
+                        {new:true},
+                    )  
+                    return updatedProductData;             
+                 }
+                 
+                 //may need to also updated category 
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        deleteProduct: async(parent, {_id}) => {
+            const product = await Product.destroy (
+                {_id},
+                {new: true},
+            )
+        },
+        
+       createCategory: async(parent, {categoryInfo}) => {
+            const categoryData = await Category.create({
+                title: categoryInfo.title,
+                tagline: categoryInfo.tagline,
+            });
+
+            let updatedCategoryData = await Category.findOneAndUpdate(
+                {_id: categoryData._id},
+                { $push: { products: categoryInfo.products}},
+                { new: true},
+            ).populate('products')
+
+            return updatedCategoryData;
+       },
+
+       deleteCategory: async (parent, {_id}) => {
+            const category = await Category.destroy(
+                { _id},
+                {new: true},
+            );
+
+            return category;
+       },
+
+       createAddress: async (parent, {addressInfo}, context) => {
+         
+        const newAddress = await Address.create({
+            streetAddress: addressInfo.streetAddress,
+            secondaryAddress: addressInfo.secondaryAddress,
+            city: addressInfo.city,
+            state: addressInfo.state,
+            zipcode: addressInfo.zipcode
+        });
+
+        if (context.user){
+            updatedUserInfo = await User.findOneAndUpdate(
+                {_id: context.user._id},
+                {$push: {address: newAddress}},
+                {new: true},
+            )
+            return updatedUserInfo
+        }
+        return newAddress;
+       }, 
+
+       
+
+       
+
+
+
 
     }
 }
